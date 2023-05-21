8f4dd5491e5c048e0963327b486a97ffd7777535
lyuboasenov <lyubo.asenov@gmail.com>
2023-05-21 11:02:46 +0300

    add mobile app initial

---
+++ app/app/PortableLoadCell/PortableLoadCell/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using PortableLoadCell.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace PortableLoadCell.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ app/app/PortableLoadCell/PortableLoadCell/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using PortableLoadCell.Models;
+using PortableLoadCell.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PortableLoadCell.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
