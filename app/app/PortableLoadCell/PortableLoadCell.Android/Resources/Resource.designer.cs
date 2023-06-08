560b900af3d8051d1ae09b483e86323fc1e9dc80
lyuboasenov <lyubo.asenov@gmail.com>
2023-06-08 20:13:51 +0300

    change logo

--- app/app/PortableLoadCell/PortableLoadCell.Android/Resources/Resource.designer.cs
+++ app/app/PortableLoadCell/PortableLoadCell.Android/Resources/Resource.designer.cs
@@ -14,7 +14,7 @@ namespace PortableLoadCell.Droid
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.2.0.93")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "1.0.0.0")]
 	public partial class Resource
 	{
 		
