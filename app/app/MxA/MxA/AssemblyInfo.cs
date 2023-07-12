a1f7d32c24f4e90307e8d982f1f1a3489c1943de
Lyuboslav Asenov <lyubo.asenov@gmail.com>
2023-07-12 23:37:50 +0300

    fix toolbar icons

--- app/app/MxA/MxA/AssemblyInfo.cs
+++ app/app/MxA/MxA/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 [assembly: ExportFont("Resources.fonts.MaterialIconsRound-Regular.otf", Alias = "MD")]
-[assembly: ExportFont("Resources.fonts.MaterialSymbolsRounded[FILL,GRAD,opsz,wght].ttf", Alias = "MD_new")]
-[assembly: ExportFont("Resources.fonts.materialdesignicons.ttf", Alias = "MD_old")]
+//[assembly: ExportFont("Resources.fonts.MaterialSymbolsRounded[FILL,GRAD,opsz,wght].ttf", Alias = "MD_new")]
+//[assembly: ExportFont("Resources.fonts.materialdesignicons.ttf", Alias = "MD_old")]
 [assembly: Preserve(AllMembers = true)]
